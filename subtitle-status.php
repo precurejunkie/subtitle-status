84e8eb4348e68ac18490b217f8c445eb72936944
Precure Junkie <precurejunkie@gmail.com>
2014-01-06 23:10:11 -0800

    Add a license header, GPLv2 to comply with Wordpress

--- subtitle-status.php
+++ subtitle-status.php
@@ -9,6 +9,22 @@ Author URI: http://twitter.com/precurejunkie
 Author Email: precurejunkie@gmail.com
 */
 
+/*  Copyright 2014  PrecureJunkie (email : precurejunkie@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 add_action( 'admin_menu', 'substatus_plugin_menu' );
 register_activation_hook( __FILE__, 'substatus_install' );
 register_activation_hook( __FILE__, 'substatus_install_data' );
